4570500fefcd91ebf22a5b396e01d73c7afebec0
frederik14 <frederik14@gmail.com>
2021-08-02 15:54:44 +0200

    ability to update ratings

--- src/app/rate-tasting/rate-tasting.component.ts
+++ src/app/rate-tasting/rate-tasting.component.ts
@@ -7,9 +7,9 @@ import { APIService } from './../API.service';
 import { Auth } from '@aws-amplify/auth';
 
 type User = {
-  username:string,
-  email:string,
-  phone:string,
+  username: string,
+  email: string,
+  phone: string,
 }
 
 @Component({
@@ -18,17 +18,19 @@ type User = {
   styleUrls: ['./rate-tasting.component.scss']
 })
 export class RateTastingComponent implements OnInit {
-  public createRating : FormGroup
+  public createRating: FormGroup
   public user: User
   public total: number
   public submitted: boolean = false
+  public rating: any
+  public rated: boolean = false
 
   constructor(
     public dialogRef: MatDialogRef<AddTastingComponent>,
     @Inject(MAT_DIALOG_DATA) public beer: Beer,
-    public formBuilder : FormBuilder,
+    public formBuilder: FormBuilder,
     public db: APIService
-  ){
+  ) {
     this.createRating = this.formBuilder.group({
       smell: [10, [Validators.required, Validators.min(0), Validators.max(10)]],
       color: [10, [Validators.required, Validators.min(0), Validators.max(10)]],
@@ -39,6 +41,7 @@ export class RateTastingComponent implements OnInit {
   }
 
   ngOnInit(): void {
+    console.log(this.beer)
     this.getUserInfo()
   }
 
@@ -49,16 +52,61 @@ export class RateTastingComponent implements OnInit {
       email: data.attributes.email,
       phone: data.attributes.phone_number
     }
+    this.rating = await this.getRating()
+    if (this.rating) {
+      this.rated = true
+      this.createRating.controls.smell.setValue(this.rating.smell)
+      this.createRating.controls.color.setValue(this.rating.color)
+      this.createRating.controls.branding.setValue(this.rating.branding)
+      this.createRating.controls.taste.setValue(this.rating.taste)
+    }
+  }
+
+  async getRating() {
+    const ratings = await this.db.ListBeerRatings(
+      {
+        userName: {
+          eq: this.user.username
+        },
+        description:{
+          ne: 'Imported from excel'
+        }
+      }, 1000)
+    for(const rating of ratings.items) {
+      if(rating.Beer.id == this.beer.id) {
+        return rating
+      }
+    }
+    return undefined
   }
 
   rateBeer() {
-    this.total = 
-    ( this.createRating.controls.smell.value
-    + this.createRating.controls.color.value
-    + this.createRating.controls.branding.value
-    + this.createRating.controls.taste.value ) * 2
+    this.total =
+      (this.createRating.controls.smell.value
+        + this.createRating.controls.color.value
+        + this.createRating.controls.branding.value
+        + this.createRating.controls.taste.value) * 2
     this.db.CreateBeerRating({
-      userName:this.user.username,
+      userName: this.user.username,
+      smell: this.createRating.controls.smell.value,
+      color: this.createRating.controls.color.value,
+      branding: this.createRating.controls.branding.value,
+      taste: this.createRating.controls.taste.value,
+      description: this.createRating.controls.taste.value,
+      beerRatingBeerId: this.beer.id
+    })
+    this.submitted = true
+  }
+
+  updateBeer() {
+    this.total =
+      (this.createRating.controls.smell.value
+        + this.createRating.controls.color.value
+        + this.createRating.controls.branding.value
+        + this.createRating.controls.taste.value) * 2
+    this.db.UpdateBeerRating({
+      id: this.beer.id,
+      userName: this.user.username,
       smell: this.createRating.controls.smell.value,
       color: this.createRating.controls.color.value,
       branding: this.createRating.controls.branding.value,
